d9a8589f8ad244fd8048c2f4b578690946feeb5e
Amir Leshem <amirl@luminate.io>
2020-02-25 11:53:51 +0200

    First release

--- projects/bdc-walkthrough-lib/src/lib/bdc-walk.module.ts
+++ projects/bdc-walkthrough-lib/src/lib/bdc-walk.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule, MatDialogModule, MatMenuModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 import {BdcWalkDialogComponent} from './tutorial-dialog/tutorial-dialog.component';
 import {BdcWalkPopupComponent} from './tutorial-popup/tutorial-popup.component';
 import {BdcWalkTriggerDirective} from './tutorial-popup/tutorial-trigger.directive';
